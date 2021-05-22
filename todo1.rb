1dc44c4387708cd15a14c20756e51d1df6d2de69
Shubham Sharma <shubham.sharma1560@gmail.com>
2021-05-22 12:37:04 +0530

    initial commit

---
+++ todo1.rb
@@ -0,0 +1,9 @@
+# ask for list of todo
+#and print them back to us
+puts "what is your next todo?"
+next_todo=gets
+puts "So this is your next todo"
+puts next_todo
+puts "\n\n\n" #carriage return
+puts "[x] #{next_todo}" #String Interpolation
+# "#{} this is ruby expression"
